4405dfd95cc48f63efb5c39ceb316847e64e841f
shahbazmansahia <shahbaz_mansahia@yahoo.com>
2021-07-03 06:28:38 -0700

    Added prefab scripts and garbage-collection scripts.

---
+++ ObjectDestroyer.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDestroyer : MonoBehaviour
+{
+    private float topBound = 40.0f;
+    private float bottomBound = -15.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }        
+        else if (transform.position.z < bottomBound)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- SahilController.cs
+++ SahilController.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class SahilController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     public float horizontalInput;
+    public float velocity = 10.0f;
+    // to determine the range which the Sahil (player) can travel
+    private float xRange = 10.0f;
+
+    // The projectile that the player will throw
+    public GameObject projectilePrefab;
+    
+        // Start is called before the first frame update
     void Start()
     {
         
@@ -14,6 +22,22 @@ public class SahilController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
         horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * velocity);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launches a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
