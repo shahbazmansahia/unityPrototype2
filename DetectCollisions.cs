00ef22071afd0f9df6c6723f6e0403103a6c9bf1
shahbazmansahia <shahbaz_mansahia@yahoo.com>
2021-07-20 04:53:22 -0700

    Added some scripts to make the code more efficient. As a part of the object pooling approach, the projectiles shot get deactivated and reactivated instead of gettting destroyed and spawned (the latter is a more resource intensive approach). the scripts replaced/changed are as follows: DetectCollisions(has been swapped from the old version; now is not my code); PlayerController (used to be sahilController; the script is still here, just not attached to the player obj anymore); DestroyOutOfBounds(my version was named 'ObjectDestroyer'; just like the player obj, this has been replaced with DestroyOutOfBounds but still exists in the scripts folder)

--- DetectCollisions.cs
+++ DetectCollisions.cs
@@ -1,28 +1,17 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DetectCollisions : MonoBehaviour
 {
-    private GameManager gameManager;
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter(Collider other)
     {
-        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        // Instead of destroying the projectile when it collides with an animal
+        //Destroy(other.gameObject); 
+
+        // Just deactivate the food and destroy the animal
+        other.gameObject.SetActive(false);
+        Destroy(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        
-        if (other.CompareTag("Animal")){
-            other.GetComponent<AnimalHunger>().FeedAnimal(1);
-            Destroy(gameObject);
-        }
-    }
 }
